6224d97782438b4edee13e8eb4248af385e7e842
“SuminLee-1” <sumin.lee707@gmail.com>
2023-10-14 03:06:00 -0400

    Start building HTML for Home page, just boxed layout

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-console.log("js is fired!");
+console.log("Js is fired!");
+
+(function(){
+	let button = document.querySelector("#button");
+	let burgerCon = document.querySelector("#burger-con");
+
+	function hamburgerMenu() {
+		burgerCon.classList.toggle("slide-toggle");
+		button.classList.toggle("expanded");
+	};
+
+	// let hamburgerMenu = () => {
+	// 	burgerCon.classList.toggle("slide-toggle");
+	// 	button.classList.toggle("expanded");
+	// };
+
+	button.addEventListener("click", hamburgerMenu, false);		
+})();
