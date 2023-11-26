f791292bd0eb5b743445f350cd4f89b829481f40
“SuminLee-1” <sumin.lee707@gmail.com>
2023-11-25 21:57:01 -0500

    change header display for desktop size

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 console.log("Js is fired!");
 
-$(document).ready(function(){
-	$(".hamburger").click(function(){
-		 $(this).toggleClass("is-active");
-		 $("#burger-con").toggleClass("show-menu"); // Toggle class to show/hide the menu
-	});
-});
+(function(){
+	let button = document.querySelector(".hamburger");
+	let burgerCon = document.querySelector("#burger-con");
+
+	function hamburgerMenu() {
+		burgerCon.classList.toggle("show-menu");
+		button.classList.toggle("is-active");
+	}
+
+	button.addEventListener("click", hamburgerMenu);
+})();
