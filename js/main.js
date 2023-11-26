7528b884412cf7d75c5845b40d21a82ed73a5ae6
“SuminLee-1” <sumin.lee707@gmail.com>
2023-11-25 20:46:54 -0500

    add some animation on the burger menu and logo

--- js/main.js
+++ js/main.js
@@ -1,18 +1,8 @@
 console.log("Js is fired!");
 
-(function(){
-	let button = document.querySelector("#button");
-	let burgerCon = document.querySelector("#burger-con");
-
-	function hamburgerMenu() {
-		burgerCon.classList.toggle("slide-toggle");
-		button.classList.toggle("expanded");
-	};
-
-	// let hamburgerMenu = () => {
-	// 	burgerCon.classList.toggle("slide-toggle");
-	// 	button.classList.toggle("expanded");
-	// };
-
-	button.addEventListener("click", hamburgerMenu, false);		
-})();
+$(document).ready(function(){
+	$(".hamburger").click(function(){
+		 $(this).toggleClass("is-active");
+		 $("#burger-con").toggleClass("show-menu"); // Toggle class to show/hide the menu
+	});
+});
