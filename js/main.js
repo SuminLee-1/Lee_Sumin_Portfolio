770c6759b229e11ecc9ab8367a7ee361a8322434
“SuminLee-1” <sumin.lee707@gmail.com>
2023-12-10 02:40:25 -0500

    build case study page

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 console.log("Js is fired!");
 
 
+// const player = new plyr("video");
+
 // burger menu
 (function(){
 	let button = document.querySelector(".hamburger");
@@ -15,6 +17,28 @@ console.log("Js is fired!");
 	button.addEventListener("click", hamburgerMenu);
 })();
 
-//contact button
+
+
+// loop text banner
+
+const banner = document.querySelector('#looping-t');
+const bannerText = banner.querySelectorAll('.infinity_inside');
+const bannerWidth = banner.offsetWidth;
+let offset = 0;
+
+function loopBanner() {
+  offset -= .1;
+  if (offset < -bannerWidth) {
+    offset = 0;
+  }
+
+  bannerText.forEach((text) => {
+    text.style.transform = `translateX(${offset}px)`;
+  });
+
+  requestAnimationFrame(loopBanner);
+}
+
+loopBanner();
 
 
--- js/scroll.js
+++ js/scroll.js
@@ -5,29 +5,18 @@ gsap.registerPlugin(ScrollToPlugin);
 
 ScrollTrigger.normalizeScroll(true)
 
-// create the smooth scroller FIRST!
-// let smoother = ScrollSmoother.create({
-//   smooth: 2,
-//   effects: true,
-//   normalizeScroll: true
-// });
 
-// pin box-c when it reaches the center of the viewport, for 300px
 ScrollTrigger.create({
   trigger: "#dec-title",
   pin: true,
   start: "top center",
-  end: "+=10"
+  end: "+=10",
+  marker: true
 });
 
 document.querySelector("button").addEventListener("click", e => {
-  // scroll to the spot where .box-c is in the center.
+
   // parameters: element, smooth, position
-  smoother.scrollTo("#dec-title", true, "center center");
+  scrollTo("#dec-title", true, "center center");
   
-  // or you could animate the scrollTop:
-  // gsap.to(smoother, {
-  //  scrollTop: smoother.offset(".box-c", "center center"),
-  //  duration: 1
-  // });
 });
