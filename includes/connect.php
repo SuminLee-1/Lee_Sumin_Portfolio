921ee38c48db6b359c8439c1bb50184f1f596806
“SuminLee-1” <sumin.lee707@gmail.com>
2024-02-27 00:00:07 -0500

    fix 500 errors

--- includes/connect.php
+++ includes/connect.php
@@ -1,7 +1,7 @@
 <?php
-$dsn = "mysql:host=localhost;dbname=erzw8445_sumin_portfolio;charset=utf8mb4";
+$dsn = "mysql:host=localhost;dbname=FIP_Portfolio;charset=utf8mb4";
 try {
-$connection = new PDO($dsn, 'erzw8445_general ', '4&sjBgXR8?*P');
+$connection = new PDO($dsn, 'root', 'root');
 } catch (Exception $e) {
   error_log($e->getMessage());
   exit('unable to connect');
